1fffd76116f1dea60d80f9af2ecd797367af9d8f
Abdullah AlGhamdi <50756573+AbdullahMu@users.noreply.github.com>
2020-09-07 19:13:41 +0300

    Update data_quality.py

--- airflow/plugins/operators/data_quality.py
+++ airflow/plugins/operators/data_quality.py
@@ -2,37 +2,52 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
+
 class DataQualityOperator(BaseOperator):
     
+    '''
+        this performs quality checks on data in our Amazon Redshift RDB after ETL
+
+        Parameters
+        ----------
+        redshift_conn_id : str
+            Amazon Redshift RDB credentials
+            
+        column_null_check : list
+            columns names which will go through futher checks, counting the numbers of nulls in them
+    '''
+
     ui_color = '#89DA59'
 
     @apply_defaults
-    def __init__(self,
-                 redshift_conn_id="", 
-                 sql_query="",
-                 expected_result="",
+    def __init__(self, 
+                 redshift_conn_id="",
+                 column_null_check=[],
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
+        # DataQualityOperator parameters:
         self.redshift_conn_id = redshift_conn_id
-        self.table_name = table_name
-        self.sql_query = sql_query # e.g. SELECT COUNT(*) FROM {self.table_name}
-        self.expected_result = expected_result
+        self.column_null_check = column_null_check
 
     def execute(self, context):
-        self.log.info("Retrieving Redshift Credentials")
-        redshift_hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
-        self.log.info("Connected to Redshift")
+        redshift_hook = PostgresHook(self.redshift_conn_id)
+        records = redshift_hook.get_records("SELECT COUNT(*) FROM public.songplays ")
+        if len(records) < 1 or len(records[0]) < 1:
+            raise ValueError("Data quality check failed. songplays table returned no results")
+        num_records = records[0][0]
+        if num_records < 1:
+            raise ValueError("Data quality check failed. songplays contained 0 rows")
+        self.log.info(f"DataQualityOperator implemented: Data quality on table songplays check passed with {records[0][0]} records")
         
-        self.log.info("Data quality check...")
-        records = redshift_hook.get_records(self.sql_query)
-        
-        if records [0][0] != self.expected_result:
-            raise ValueError(f"""
-                Data quality check has failed. \
-                {results[0][0]} does not equal {self.expected_result}
-            """)
-        else:
-            self.log.info("Data quality check has passed")
-        
-        
+        if column_null_check:
+            
+            null_records = 0
+            
+            for column in column_null_check:
+                null_query = f'''
+                                 SELECT COUNT(*) FROM public.songplays WHERE {column} IS NULL
+                             '''
+                null_records += redshift_hook.get_records(null_query)
+                
+        self.log.info(f"table songplays has {null_records[0][0]} null records")
